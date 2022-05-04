cd1803cbbd7b4dadef46432637eabac1d43d835a
alexis1711 <alexis101cornejo@gmail.com>
2022-05-04 01:36:52 -0600

    back y front

--- CrudApi-Lis104/routes/api.php
+++ CrudApi-Lis104/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/articulos', [ArticuloController::class,'index']);
+Route::post('/articulos', [ArticuloController::class,'store']);
+Route::put('/articulos/{id}', [ArticuloController::class,'update']);
+Route::delete('/articulos/{id}', [ArticuloController::class,'destroy']);
